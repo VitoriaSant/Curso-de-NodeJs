3b5ff2df272a3be817d2a82ce9cb9dc40b1920f2
VitoriaSant <svitoria3465@gamail.com>
2023-08-17 17:28:31 -0300

    Parametros

--- Express/index.js
+++ Express/index.js
@@ -2,7 +2,18 @@ const express = require("express"); // importando o Express
 const app = express(); // iniciando o express e passando para a contante app
 
 app.get("/",function(req,res){
-    
+    res.send("Servidor rodando"); // So é possivel enviar uma resposta somente uma vez
+})
+
+app.get("/blog", function(req,res){
+    res.send("Bem vindo ao Blog");
+})
+
+app.get("/ola/:nome/:empresa", function(req,res){
+    var nome = req.params.nome;
+    var empresa = req.params.empresa;
+
+    res.send("Olá "+ nome + " da empresa "+ empresa +"");
 })
 
 app.listen(4000,function(erro){
