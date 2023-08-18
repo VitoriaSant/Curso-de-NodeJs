3fca2dfbf84d916cfda50a9cad3b435f4b1dca24
VitoriaSant <svitoria3465@gamail.com>
2023-08-18 10:24:09 -0300

    Parametros Obrigt, não obrigt e Query Params

--- Express/index.js
+++ Express/index.js
@@ -5,15 +5,34 @@ app.get("/",function(req,res){
     res.send("Servidor rodando"); // So é possivel enviar uma resposta somente uma vez
 })
 
-app.get("/blog", function(req,res){
-    res.send("Bem vindo ao Blog");
+// Parametros não obrigatórios
+app.get("/blog/:artigo?", function(req,res){
+    var artigo = req.params.artigo;
+
+    if(artigo){
+        res.send("Bem vindo ao artigo: " + artigo +"");
+    }else{
+        res.send("Bem vindo ao Blog");
+    }
 })
 
+//Parametros obrigatórios
 app.get("/ola/:nome/:empresa", function(req,res){
     var nome = req.params.nome;
     var empresa = req.params.empresa;
 
     res.send("Olá "+ nome + " da empresa "+ empresa +"");
+})
+
+//Query Params
+app.get("/canal/youtube", function(req,res){
+    var canal = req.query["canal"];
+
+    if(canal){
+        res.send("Canal " + canal +"");
+    }else{
+        res.send("Nenhum canal");
+    }
 })
 
 app.listen(4000,function(erro){
